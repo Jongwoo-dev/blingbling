6930a0e522b1fb9471727f05f14b6b60bc27ef47
Jongwoo.dev <Jongwoo.dev@gmail.com>
2017-02-28 11:51:48 +0900

    상품 데이터 정보 변경한 테이블에 맞도록 수정

--- data-상품.sql
+++ data-상품.sql
@@ -1,13 +1,13 @@
 -- Item
-insert into item(ino, msno, name, price, utime, otime, ctime) values(1, 5, '전립선마사지', 50000, 30,'10:00:00','20:00:00');
-insert into item(ino, msno, name, price, utime, otime, ctime) values(2, 6, '짦은밤', 100000, 120,'00:00:00','24:00:00');
-insert into item(ino, msno, name, price, utime, otime, ctime) values(3, 6, '긴밤', 200000, 600,'00:00:00','24:00:00');
-insert into item(ino, msno, name, price, utime, otime, ctime) values(4, 7, '건마', 80000, 50,'00:00:00','24:00:00');
-insert into item(ino, msno, name, price, utime, otime, ctime) values(5, 8, '불건마', 200000, 120,'00:00:00','24:00:00');
-insert into item(ino, msno, name, price, utime, otime, ctime) values(6, 8, 'longbam', 200000, 120,'00:00:00','24:00:00');
-insert into item(ino, msno, name, price, utime, otime, ctime) values(7, 8, '건강마사지', 150000, 60,'00:00:00','24:00:00');
-insert into item(ino, msno, name, price, utime, otime, ctime) values(8, 7, '건강마사지', 150000, 60,'00:00:00','24:00:00');
-insert into item(ino, msno, name, price, utime, otime, ctime) values(9, 7, '힐링테라피', 200000, 60,'00:00:00','24:00:00');
-insert into item(ino, msno, name, price, utime, otime, ctime) values(10, 7, '건강마사지', 150000, 60,'00:00:00','24:00:00');
-insert into item(ino, msno, name, price, utime, otime, ctime) values(11, 7, '다이어트마사지', 150000, 60,'00:00:00','24:00:00');
-insert into item(ino, msno, name, price, utime, otime, ctime) values(12, 7, '힐링마사지', 150000, 60,'00:00:00','24:00:00');
+insert into item(ino, msno, name, price, utime) values(1, 5, '전립선마사지', 50000, 30);
+insert into item(ino, msno, name, price, utime) values(2, 6, '짦은밤', 100000, 120);
+insert into item(ino, msno, name, price, utime) values(3, 6, '긴밤', 200000, 600);
+insert into item(ino, msno, name, price, utime) values(4, 7, '건마', 80000, 50);
+insert into item(ino, msno, name, price, utime) values(5, 8, '불건마', 200000, 120);
+insert into item(ino, msno, name, price, utime) values(6, 8, 'longbam', 200000, 120);
+insert into item(ino, msno, name, price, utime) values(7, 8, '건강마사지', 150000, 60);
+insert into item(ino, msno, name, price, utime) values(8, 7, '건강마사지', 150000, 60);
+insert into item(ino, msno, name, price, utime) values(9, 7, '힐링테라피', 200000, 60);
+insert into item(ino, msno, name, price, utime) values(10, 7, '건강마사지', 150000, 60);
+insert into item(ino, msno, name, price, utime) values(11, 7, '다이어트마사지', 150000, 60);
+insert into item(ino, msno, name, price, utime) values(12, 7, '힐링마사지', 150000, 60);
