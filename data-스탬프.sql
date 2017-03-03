b32e7badfff8ad1d5a2e4a5788bade69d986b774
Jongwoo.dev <Jongwoo.dev@gmail.com>
2017-03-03 15:11:31 +0900

    스템프 자료 수정

--- data-스탬프.sql
+++ data-스탬프.sql
@@ -1,18 +1,20 @@
 -- 스탬프
 insert into stmp(mno, msno, cnt) values(1,5,10);
-insert into stmp_log(mno, msno, chng, pm, chdt) values(1,5,1,-9,now());
 insert into stmp(mno, msno, cnt) values(2,6,4);
-insert into stmp_log(mno, msno, chng, pm, chdt) values(2,6,3,-1,now());
 insert into stmp(mno, msno, cnt) values(3,7,3);
-insert into stmp_log(mno, msno, chng, pm, chdt) values(3,7,10,+7,now());
 insert into stmp(mno, msno, cnt) values(4,7,5);
-insert into stmp_log(mno, msno, chng, pm, chdt) values(4,7,4,-1,now());
-insert into stmp_log(mno, msno, chng, pm, chdt) values(4,7,10,+6,now());
 insert into stmp(mno, msno, cnt) values(1,6,6);
-insert into stmp_log(mno, msno, chng, pm, chdt) values(1,6,4,-2,now());
-insert into stmp_log(mno, msno, chng, pm, chdt) values(1,6,10,+6,now());
 insert into stmp(mno, msno, cnt) values(2,7,7);
-insert into stmp_log(mno, msno, chng, pm, chdt) values(2,7,10,+3,now());
+
+-- 스탬프 로그
+insert into stmp_log(slno, mno, msno, chng, pm, chdt) values(1, 1,5,1,-9,now());
+insert into stmp_log(slno, mno, msno, chng, pm, chdt) values(2, 2,6,3,-1,now());
+insert into stmp_log(slno, mno, msno, chng, pm, chdt) values(3, 3,7,10,+7,now());
+insert into stmp_log(slno, mno, msno, chng, pm, chdt) values(4, 4,7,4,-1,now());
+insert into stmp_log(slno, mno, msno, chng, pm, chdt) values(5, 4,7,10,+6,now());
+insert into stmp_log(slno, mno, msno, chng, pm, chdt) values(6, 1,6,4,-2,now());
+insert into stmp_log(slno, mno, msno, chng, pm, chdt) values(7, 1,6,10,+6,now());
+insert into stmp_log(slno, mno, msno, chng, pm, chdt) values(8, 2,7,10,+3,now());
 
 -- 업체스탬프
 insert into stmp_bnf(sbno, msno, sb_cnt, sb_cont) values(1 ,5, 15,'마사지');
