c93bac10860cdc9d55106d84de2d76e9d02acd01
Jongwoo.dev <Jongwoo.dev@gmail.com>
2017-03-03 14:59:03 +0900

    아이템관리 수정

--- data-스탬프.sql
+++ data-스탬프.sql
@@ -14,12 +14,12 @@ insert into stmp_log(mno, msno, chng, pm, chdt) values(1,6,10,+6,now());
 insert into stmp(mno, msno, cnt) values(2,7,7);
 insert into stmp_log(mno, msno, chng, pm, chdt) values(2,7,10,+3,now());
 
---업체스탬프
-insert into stmp_bnf(msno, sb_cnt, sb_cont) values(5, 15,'마사지');
-insert into stmp_bnf(msno, sb_cnt, sb_cont) values(5, 10,'네일아트');
-insert into stmp_bnf(msno, sb_cnt, sb_cont) values(6, 15,'피부관리');
-insert into stmp_bnf(msno, sb_cnt, sb_cont) values(6, 15,'전신마사지');
-insert into stmp_bnf(msno, sb_cnt, sb_cont) values(7, 5,'네일아트');
-insert into stmp_bnf(msno, sb_cnt, sb_cont) values(7, 10,'마사지');
+-- 업체스탬프
+insert into stmp_bnf(sbno, msno, sb_cnt, sb_cont) values(1 ,5, 15,'마사지');
+insert into stmp_bnf(sbno, msno, sb_cnt, sb_cont) values(2 ,5, 10,'네일아트');
+insert into stmp_bnf(sbno, msno, sb_cnt, sb_cont) values(3 ,6, 15,'피부관리');
+insert into stmp_bnf(sbno, msno, sb_cnt, sb_cont) values(4 ,6, 15,'전신마사지');
+insert into stmp_bnf(sbno, msno, sb_cnt, sb_cont) values(5 ,7, 5,'네일아트');
+insert into stmp_bnf(sbno, msno, sb_cnt, sb_cont) values(6 ,7, 10,'마사지');
 
 
