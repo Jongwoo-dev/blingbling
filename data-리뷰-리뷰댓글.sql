eaafeed2c5ade6fc05493c69f43f9350db1fc7ed
Jongwoo.dev <Jongwoo.dev@gmail.com>
2017-03-13 21:10:02 +0900

    댓글 읽어오는것까지 완료. 쓰는거 연결 필요

--- data-리뷰-리뷰댓글.sql
+++ data-리뷰-리뷰댓글.sql
@@ -5,5 +5,5 @@ insert into review(mno,msno,fsco,ssco,csco) values(1,5,8,9,10);
 insert into rv_cmt(rvno,mno,msno,grup,level,seq,cont,rc_rdt) values(1,1,5,1,1,1,'자주가는데 갈때마다 너무 좋아요~!',now());
 insert into rv_cmt(rvno,mno,msno,grup,level,seq,cont,rc_rdt) values(2,5,5,1,2,2,'이용해 주셔서 감사합니다.',now());
 insert into rv_cmt(rvno,mno,msno,grup,level,seq,cont,rc_rdt) values(3,1,5,2,1,1,'자주가는데 갈때마다 너무 좋아요~!\n자주가는데 갈때마다 너무 좋아요~!\n자주가는데 갈때마다 너무 좋아요~!',now());
-insert into rv_cmt(rvno,mno,msno,grup,level,seq,cont,rc_rdt) values(4,1,5,2,2,2,'이용해 주셔서 감사합니다.',now());
+insert into rv_cmt(rvno,mno,msno,grup,level,seq,cont,rc_rdt) values(4,5,5,2,2,2,'이용해 주셔서 감사합니다.',now());
 
--- src/main/webapp/uitest/comment.js
+++ src/main/webapp/uitest/comment.js
@@ -1,3 +1,16 @@
+var loginMember;
+var corporateNo;
+
+try {
+	var splitParam = location.href.split('?')[1].split('=');
+	if (splitParam[0] == 'corporateNo') {
+		corporateNo = splitParam[1].replace(/#/gi, '');
+	} else {
+		corporateNo = -1;
+	}
+} catch (error) {
+	var corporateNo = -1;
+}
 
 $(function() {
 	$.getJSON('../auth/loginUser.json', function(ajaxResult) {
@@ -13,15 +26,37 @@ $(function() {
 });
 
 var initInfo = function() {
-	/*$.getJSON('../corporate/detail.json?memberNo='+loginMember.memberNo, function(ajaxResult) {
+	$.getJSON('/blingbling/comment/list.json?corporateNo='+corporateNo, function(ajaxResult) {
 		if (ajaxResult.status != 'success') {
-			alert('업체가 아닙니다.');
+			swal('오류!', ajaxResult.data, 'error')
 			return;
 		}
 		
-		var corporate = ajaxResult.data;
-		$('#infoEditor').summernote('code', corporate.priceTime);
-	});*/
-	
+		var list = ajaxResult.data;
+		console.log(list);
+		
+		var comment = new Array();
+		/*var reply = new Array();*/
+		var contentDiv = $('#content');
+		
+		var commentTemplate = Handlebars.compile($('#commentBoxTemplate').html());
+		var replyTemplate = Handlebars.compile($('#replyBoxTemplate').html());
+		var writeTemplate = Handlebars.compile($('#writeBoxTemplate').html());
+		
+		$('<div>')
+			.addClass('comment-box')
+			.html(writeTemplate(loginMember))
+			.appendTo(contentDiv);
+		
+		for (var i in list) {
+			if (list[i].level == 1) {
+				$('#content').append(commentTemplate(list[i])).appendTo(contentDiv);
+				comment[list[i].group] = $('.comment-box').last();
+			} else {
+				comment[list[i].group].append(replyTemplate(list[i]));
+			}
+		}
+		console.log(comment);
+	});
 }
 
