0619d2f07a9103a8e349d0cdbb73a331408f5e79
dehan <dehan@DESKTOP-I484O6E>
2017-03-16 15:01:35 +0900

    detail-info

--- src/main/webapp/corporate/detail-info.js
+++ src/main/webapp/corporate/detail-info.js
@@ -3,9 +3,76 @@ var marker;
 var infowindow;
 var loginMember;
 var filename;
+var memberNo = 5;
 
+$('#favorite-star-span').click(function(){
+	$(this).addClass('glyphicon glyphicon-star');
+});
 
-$.getJSON('../corporate/detail.json?memberNo=5', function(ajaxResult) {
+function initMap() {
+	  var currentLatLng;
+	  map = new google.maps.Map(document.getElementById('CorporaterMap'), {
+	    center: {lat: 37.5666805, lng: 126.9784147},
+	    zoom: 17
+	  });
+	  marker = new google.maps.Marker({
+	    //position: {lat: 37.5666805, lng: 126.9784147},
+	    map: map,
+	    draggable:false,
+	    title:"업체의 위치로 드래그 해 주세요"
+	  });
+	  infowindow = new google.maps.InfoWindow({
+	    content: ''
+	  });
+	 /* google.maps.event.addListener(map, 'click', function(mouseEvent) {
+		  moveMarker(mouseEvent.latLng);
+	  });*/
+	  
+	  /*marker.addListener('mouseup', function(mouseEvent) {
+		  moveMarker(mouseEvent.latLng);
+	  });*/
+	}
+
+	var moveMarker = function(latlng) {
+
+		var geocoder = new google.maps.Geocoder();
+		geocoder.geocode({
+			latLng: latlng
+		}, function(results, status) {
+			if (status == google.maps.GeocoderStatus.OK) {
+				if (results[0].geometry) {
+					var address = results[0].formatted_address;
+					window.marker.setPosition(latlng)
+					infowindow.setContent(address)
+					infowindow.open(map, marker);
+
+					var opt = $("<option value='" + latlng.toString() + "'>" + address + "</option>");
+					$("#markerList").append(opt);
+				}
+			} else if (status == google.maps.GeocoderStatus.ERROR) {
+				alert("통신중 에러발생！");
+			} else if (status == google.maps.GeocoderStatus.INVALID_REQUEST) {
+				alert("요청에 문제발생！geocode()에 전달하는GeocoderRequest확인요！");
+			} else if (status == google.maps.GeocoderStatus.OVER_QUERY_LIMIT) {
+				alert("단시간에 쿼리 과다송신！");
+			} else if (status == google.maps.GeocoderStatus.REQUEST_DENIED) {
+				alert("이 페이지에는 지오코더 이용 불가! 왜??");
+			} else if (status == google.maps.GeocoderStatus.UNKNOWN_ERROR) {
+				alert("서버에 문제가 발생한거 같아요. 다시 한번 해보세요.");
+			} else if (status == google.maps.GeocoderStatus.ZERO_RESULTS) {
+				alert("존재하지 않습니다.");
+			} else {
+				alert("??");
+			}
+		});
+	}
+
+	var initMarker = function(latlngStr) {
+		var point = latlngStr.substring(1,latlngStr.length-1).split(',');
+		moveMarker(new google.maps.LatLng({lat:Number(point[0]), lng:Number(point[1])}));
+	}
+
+$.getJSON('../corporate/detail.json?memberNo=' + memberNo, function(ajaxResult) {
 	if (ajaxResult.status != 'success') {
 		alert('업체가 아닙니다.');
 		return;
@@ -15,7 +82,45 @@ $.getJSON('../corporate/detail.json?memberNo=5', function(ajaxResult) {
 		$('#corp-name-span').text(corporate.corporateName);
 		$('#corp-addr-span').text(corporate.baseAddress +" " + corporate.detailAddress);
 		$('#corp-tel-span').text(corporate.tel);
-		$('#info-cont-div').html(corporate.additionalInfo);
+		$('#comment-span').html(corporate.additionalInfo);
+		$('#navi-header-div').text('정보&사진');
+		$('#info-cont-div').html(corporate.detail);
+		
+		$('#infonpic-btn').click(function() {
+			$('button').removeClass('navi-selected');
+			$(this).addClass('navi-selected');
+			$('#navi-header-div').text('정보&사진');
+			$('#info-cont-div').html(corporate.detail);
+		});
+		
+		$('#feentime-btn').click(function() {
+			$('button').removeClass('navi-selected');
+			$(this).addClass('navi-selected');
+			$('#navi-header-div').text('요금 및 시간');
+			$('#info-cont-div').html(corporate.priceTime);
+		});
+		
+		$('#notice-btn').click(function() {
+			$('button').removeClass('navi-selected');
+			$(this).addClass('navi-selected');
+			$('#navi-header-div').text('공지사항');
+			$('#info-cont-div').text(corporate.notice);
+		});
+		
+		$('#review-btn').click(function() {
+			$('button').removeClass('navi-selected');
+			$(this).addClass('navi-selected');
+			$('#navi-header-div').text('리뷰');
+			$('#info-cont-div').text(corporate.notice);
+		});
+		
+		
+		if (corporate.mapLocation != null) {
+			if (corporate.mapLocation.length != 0) {
+				initMarker(corporate.mapLocation);
+			}
+		}
+		
 /*
 	
 	$('#infoEditor').summernote('code', corporate.additionalInfo)
@@ -45,78 +150,17 @@ $.getJSON('../corporate/detail.json?memberNo=5', function(ajaxResult) {
 	}
 */
 	
-});
+
 
 /*$('#booking-btn').click(function() {
 	reservation = window.open("detail.html", "mywindow");
 
 });*/
+
+});
+
+
 /*
-function initMap() {
-  var currentLatLng;
-  map = new google.maps.Map(document.getElementById('CorporaterMap'), {
-    center: {lat: 37.5666805, lng: 126.9784147},
-    zoom: 17
-  });
-  marker = new google.maps.Marker({
-    //position: {lat: 37.5666805, lng: 126.9784147},
-    map: map,
-    draggable:true,
-    title:"업체의 위치로 드래그 해 주세요"
-  });
-  infowindow = new google.maps.InfoWindow({
-    content: ''
-  });
-  google.maps.event.addListener(map, 'click', function(mouseEvent) {
-	  moveMarker(mouseEvent.latLng);
-  });
-  
-  marker.addListener('mouseup', function(mouseEvent) {
-	  moveMarker(mouseEvent.latLng);
-  });
-}
-
-var moveMarker = function(latlng) {
-
-	var geocoder = new google.maps.Geocoder();
-	geocoder.geocode({
-		latLng: latlng
-	}, function(results, status) {
-		if (status == google.maps.GeocoderStatus.OK) {
-			if (results[0].geometry) {
-				var address = results[0].formatted_address;
-				window.marker.setPosition(latlng)
-				infowindow.setContent(address)
-				infowindow.open(map, marker);
-
-				var opt = $("<option value='" + latlng.toString() + "'>" + address + "</option>");
-				$("#markerList").append(opt);
-			}
-		} else if (status == google.maps.GeocoderStatus.ERROR) {
-			alert("통신중 에러발생！");
-		} else if (status == google.maps.GeocoderStatus.INVALID_REQUEST) {
-			alert("요청에 문제발생！geocode()에 전달하는GeocoderRequest확인요！");
-		} else if (status == google.maps.GeocoderStatus.OVER_QUERY_LIMIT) {
-			alert("단시간에 쿼리 과다송신！");
-		} else if (status == google.maps.GeocoderStatus.REQUEST_DENIED) {
-			alert("이 페이지에는 지오코더 이용 불가! 왜??");
-		} else if (status == google.maps.GeocoderStatus.UNKNOWN_ERROR) {
-			alert("서버에 문제가 발생한거 같아요. 다시 한번 해보세요.");
-		} else if (status == google.maps.GeocoderStatus.ZERO_RESULTS) {
-			alert("존재하지 않습니다.");
-		} else {
-			alert("??");
-		}
-	});
-}
-
-var initMarker = function(latlngStr) {
-	var point = latlngStr.substring(1,latlngStr.length-1).split(',');
-	moveMarker(new google.maps.LatLng({lat:Number(point[0]), lng:Number(point[1])}));
-}
-
-
-
 $("#add-telform").click(function() {
 	
 	if($("#tel-group").children().length > 4) {
