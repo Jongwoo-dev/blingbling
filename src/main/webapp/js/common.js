953590ce368100b6c26f265631cce59ab796a3f3
Jongwoo.dev <Jongwoo.dev@gmail.com>
2017-03-17 12:37:55 +0900

    .

--- src/main/webapp/js/common.js
+++ src/main/webapp/js/common.js
@@ -32,11 +32,41 @@ $(function() {
 					.html("<a href='#' class='header-menu-a' data-toggle='modal' data-target='#myModal'>로그인</a>");
 				return;
 			}
+			member = ajaxResult.data;
+			//console.log(member);
 			// 로그인 되었으면 로그인한 유저 정보로 헤더 초기화
 			// *서버에 이미지가 없을시 엑박 대체할 이미지 필요함. 나중에 추가
-			
-			
-			
+
+			// 이름변경
+			$('#login-name-div').css('position','relative').children().remove();
+			$('<a>').attr('href','#').addClass('header-menu-a').addClass('dropdown-toggle')
+				.attr('id','login-name-a').attr('data-toggle','dropdown')
+				.attr('aria-haspopup','true').attr('aria-expanded','true')
+				.text(member.name).css('color','#337ab7')
+				.appendTo('#login-name-div');
+
+			// 드롭박스 추가 및 클릭이벤트 추가
+			var ul = $('<ul>').addClass('dropdown-menu').addClass('dropdown-menu')
+				.attr('aria-labelledby','login-name-a').appendTo('#login-name-div');
+			$('<li>').html('<a href="#" id="login-dropdown-mypage" class="header-menu-a">마이 페이지</a>')
+				.appendTo(ul).click(function(e) {
+					e.preventDefault();
+					alert('임시로 찜 페이지에 넣음. 나중에 내정보수정 페이지로 변경'); 
+					location.href = '/blingbling/mypage/favor.html';
+				});
+			if (member.type == 'ceo') {
+				$('<li>').html('<a href="#" id="login-dropdown-corppage" class="header-menu-a">업체 관리자 페이지</a>')
+					.appendTo(ul).click(function(e) {
+						e.preventDefault();
+						location.href = '/blingbling/corporate/basicinfo.html';
+					});
+			} else if (member.type == 'admin') {
+				$('<li>').html('<a href="#" id="login-dropdown-adminpage" class="header-menu-a">운영자 페이지</a>')
+					.appendTo(ul).click(function(e) {
+						e.preventDefault();
+						location.href = '/blingbling/admin/admin-qna.html';
+					});
+			}
 		});
 	});
 });
